10b1000965945eb79e10492a588d7ac730ddb011
Brittany Walker <breneewalker@gmail.com>
2021-02-18 00:09:41 -0500

    Add fontawesome

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faStar);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
