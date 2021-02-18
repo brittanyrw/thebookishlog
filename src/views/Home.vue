856ebb141046cc4a04ca85335625ef39c71dc8ae
Brittany Walker <breneewalker@gmail.com>
2021-02-18 02:54:37 -0500

    Update sidebar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,27 @@
 <template>
   <div class="home">
     <div class="sidebar">
-      <h1>Bookish Log</h1>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. At, iste vel
-        consequuntur aut excepturi ratione, eum minus fugit perferendis sequi
-        quidem voluptas aliquid. Qui asperiores quasi provident molestias, quis
-        ex?
-      </p>
+      <div class="sidebar-top">
+        <h1>Bookish Log</h1>
+        <p>
+          A log of all of the books I have read starting in 2021.
+        </p>
+      </div>
+      <div class="sidebar-navigation">
+        <router-link to="/authors"><button>Authors</button></router-link>
+      </div>
+      <div class="sidebar-bottom">
+        <p>
+          Created By
+          <a href="https://musicalwebdev.com" target="_blank">
+            Brittany Walker</a
+          >
+        </p>
+        <h4>Other Logs</h4>
+        <ul>
+          <li>Theater Log</li>
+        </ul>
+      </div>
     </div>
     <div class="main">
       <OverallBookStatistics :book-info="books" />
@@ -45,6 +59,7 @@ export default {
 
 <style lang="scss">
 @import "@/assets/styles/varibles.scss";
+@import url("https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap");
 
 .home {
   display: grid;
@@ -58,11 +73,37 @@ export default {
 .main {
   padding: 20px;
   border: 10px solid $black;
+  border-top: 0;
+}
+
+.main::before {
+  background: linear-gradient(
+    to right,
+    $yellow 16.66%,
+    $orange 16.66%,
+    $orange 33.32%,
+    $red 33.32%,
+    $red 49.98%,
+    $green 49.98%,
+    $green 66.64%,
+    $blue 66.64%,
+    $blue 83.3%,
+    $purple 83.3%
+  );
+  position: absolute;
+  content: "";
+  height: 20px;
+  right: 0;
+  left: 0;
+  top: 0;
 }
 
 .sidebar {
   background-color: $black;
   color: white;
   padding: 20px;
+  h1 {
+    font-family: "Abril Fatface", cursive;
+  }
 }
 </style>
