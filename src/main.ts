2ad25ca07c493570c64c7108b14b34cab0d77aaf
Brittany Walker <breneewalker572@gmail.com>
2021-08-23 15:03:02 -0400

    Add font awesome

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+
+library.add(fas, far)
 
 createApp(App)
   .use(router)
+  .component('font-awesome-icon', FontAwesomeIcon)
   .mount("#app");
