53afdf77e2507f519aa24c1996ef7251e994d238
Brittany Walker <breneewalker@gmail.com>
2023-04-21 00:40:50 -0400

    initial commit

---
+++ src/data/authors.js
@@ -0,0 +1,658 @@
+export const authorData = [
+  {
+    name: "Erica Armstrong Dunbar",
+    books: ["She Came to Slay: The Life and Times of Harriet Tubman"],
+    poc: true,
+    gender: "female",
+    lgbt: false,
+    fav: false,
+    country: [
+      {
+        code: "US",
+        name: "United States"
+      }
+    ],
+    website: "ericaarmstrongdunbar.com"
+  },
+  {
+    name: "Kalynn Bayron",
+    books: ["Cinderella Is Dead", "This Poison Heart", "This Wicked Fate"],
+    poc: true,
+    gender: "female",
+    lgbt: true,
+    fav: false,
+    country: [
+      {
+        code: "US",
+        name: "United States"
+      }
+    ],
+    website: "kalynnbayron.com"
+  },
+  {
+    name: "Alexis Henderson",
+    books: ["The Year of the Witching"],
+    poc: true,
+    gender: "female",
+    lgbt: false,
+    fav: false,
+    country: [
+      {
+        code: "US",
+        name: "United States"
+      }
+    ],
+    website: "alexishenderson.com"
+  },
+  {
+    name: "Tiffany D. Jackson",
+    books: [
+      "Monday's Not Coming",
+      "Allegedly",
+      "Blackout",
+      "Grown",
+      "Let Me Hear a Rhyme",
+      "Whiteout",
+      "The Weight of Blood",
+      "White Smoke"
+    ],
+    poc: true,
+    gender: "female",
+    lgbt: false,
+    fav: true,
+    country: [
+      {
+        code: "US",
+        name: "United States"
+      }
+    ],
+    website: "writeinbk.com"
+  },
+  {
+    name: "Kimberly Jones",
+    books: ["I'm Not Dying with You Tonight"],
+    poc: true,
+    gender: "female",
+    lgbt: false,
+    fav: false,
+    country: [
+      {
+        code: "US",
+        name: "United States"
+      }
+    ],
+    website: "kimjoneswrites.com"
+  },
+  {
+    name: "Gilly Segal",
+    books: ["I'm Not Dying with You Tonight"],
+    poc: false,
+    gender: "female",
+    lgbt: false,
+    fav: false,
+    country: [
+      {
+        code: "US",
+        name: "United States"
+      }
+    ],
+    website: "gillysegal.com"
+  },
+  {
+    name: "Camryn Garrett",
+    books: ["Full Disclosure"],
+    poc: true,
+    gender: "female",
+    lgbt: true,
+    fav: false,
+    country: [
+      {
+        code: "US",
+        name: "United States"
+      }
+    ],
+    website: "camryngarrett.com"
+  },
+  {
+    name: "Hallie Rubenhold",
+    books: [
+      "The Five: The Untold Lives of the Women Killed by Jack the Ripper"
+    ],
+    poc: false,
+    gender: "female",
+    lgbt: false,
+    fav: false,
+    country: [
+      {
+        code: "GB",
+        name: "United Kingdom"
+      }
+    ],
+    website: "hallierubenhold.com"
+  },
+  {
+    name: "Silvia Moreno-Garcia",
+    books: ["Gods of Jade and Shadow"],
+    poc: false,
+    gender: "female",
+    lgbt: false,
+    fav: false,
+    country: [
+      {
+        code: "MX",
+        name: "Mexico"
+      },
+      {
+        code: "CA",
+        name: "Canada"
+      }
+    ],
+    website: "silviamoreno-garcia.com"
+  },
+  {
+    name: "Cho Nam-Joo",
+    books: ["Kim Jiyoung, Born 1982"],
+    poc: true,
+    gender: "female",
+    lgbt: false,
+    fav: false,
+    country: [
+      {
+        code: "KR",
+        name: "Korea"
+      }
+    ],
+    website: ""
+  },
+  {
+    name: "Kim Liggett",
+    books: ["The Grace Year"],
+    poc: false,
+    gender: "female",
+    lgbt: false,
+    fav: false,
+    country: [
+      {
+        code: "US",
+        name: "United States"
+      }
+    ],
+    website: "kimliggett.com"
+  },
+  {
+    name: "P. Djèlí Clark",
+    books: ["Ring Shout", "A Master of Djinn", "The Black God's Drums"],
+    poc: true,
+    gender: "male",
+    lgbt: false,
+    fav: true,
+    country: [
+      {
+        code: "TT",
+        name: "Trinidad and Tobago"
+      },
+      {
+        code: "US",
+        name: "United States"
+      }
+    ],
+    website: "pdjeliclark.com"
+  },
+  {
+    name: "Alexis Hall",
+    books: ["Boyfriend Material"],
+    poc: false,
+    gender: "male",
+    lgbt: true,
+    fav: false,
+    country: [
+      {
+        code: "GB",
+        name: "United Kingdom"
+      }
+    ],
+    website: "quicunquevult.com",
+    image: false
+  },
+  {
+    name: "Brit Bennett",
+    books: ["The Vanishing Half"],
+    poc: true,
+    gender: "female",
+    lgbt: false,
+    fav: true,
+    country: [
+      {
+        code: "US",
+        name: "United States"
+      }
+    ],
+    website: "britbennett.com"
+  },
+  {
+    name: "Jordan Ifueko",
+    books: ["Raybearer", "Redemptor"],
+    poc: true,
+    gender: "female",
+    lgbt: false,
+    fav: false,
+    country: [
+      {
+        code: "NG",
+        name: "Nigeria"
+      },
+      {
+        code: "US",
+        name: "United States"
+      }
+    ],
+    website: "jordanifueko.com"
+  },
+  {
+    name: "Kiley Reid",
+    books: ["Such a Fun Age"],
+    poc: true,
+    gender: "female",
+    lgbt: false,
+    fav: false,
+    country: [
+      {
+        code: "US",
+        name: "United States"
+      }
+    ],
+    website: "kileyreid.com"
+  },
+  {
+    name: "Dhonielle Clayton",
+    books: ["Blackout", "Whiteout"],
+    poc: true,
+    gender: "female",
+    lgbt: false,
+    fav: false,
+    country: [
+      {
+        code: "US",
+        name: "United States"
+      }
+    ],
+    website: "dhonielleclayton.com"
+  },
+  {
+    name: "Nic Stone",
+    books: ["Blackout", "Whiteout"],
+    poc: true,
+    gender: "female",
+    lgbt: true,
+    fav: false,
+    country: [
+      {
+        code: "US",
+        name: "United States"
+      }
+    ],
+    website: "nicstone.info"
+  },
+  {
+    name: "Angie Thomas",
+    books: ["Blackout", "Whiteout"],
+    poc: true,
+    gender: "female",
+    lgbt: false,
+    fav: false,
+    country: [
+      {
+        code: "US",
+        name: "United States"
+      }
+    ],
+    website: "angiethomas.com"
+  },
+  {
+    name: "Ashley Woodfolk",
+    books: ["Blackout", "Whiteout"],
+    poc: true,
+    gender: "female",
+    lgbt: true,
+    fav: false,
+    country: [
+      {
+        code: "US",
+        name: "United States"
+      }
+    ],
+    website: "ashleywoodfolk.com"
+  },
+  {
+    name: "Nicola Yoon",
+    books: ["Blackout", "Whiteout"],
+    poc: true,
+    gender: "female",
+    lgbt: false,
+    fav: false,
+    country: [
+      {
+        code: "JM",
+        name: "Jamaica"
+      },
+      {
+        code: "US",
+        name: "United States"
+      }
+    ],
+    website: "nicolayoon.com"
+  },
+  {
+    name: "Nina Varela",
+    books: ["Crier's War", "Iron Heart"],
+    poc: false,
+    gender: "female",
+    lgbt: true,
+    fav: false,
+    country: [
+      {
+        code: "US",
+        name: "United States"
+      }
+    ],
+    website: "ninavarela.com"
+  },
+  {
+    name: "Justina Ireland",
+    books: ["Dread Nation", "Deathless Divide", "Rust in the Root"],
+    poc: true,
+    gender: "female",
+    lgbt: true,
+    fav: false,
+    country: [
+      {
+        code: "US",
+        name: "United States"
+      }
+    ],
+    website: "authorjustinaireland.com"
+  },
+  {
+    name: "Faridah Àbíké-Íyímídé",
+    books: ["Ace of Spades"],
+    poc: true,
+    gender: "female",
+    lgbt: true,
+    fav: false,
+    country: [
+      {
+        code: "GB",
+        name: "United Kingdom"
+      },
+      {
+        code: "NG",
+        name: "Nigeria"
+      }
+    ],
+    website: "faridahabikeiyimide.com"
+  },
+  {
+    name: "N.K. Jemisin",
+    books: [
+      "How Long 'til Black Future Month?",
+      "The City We Became",
+      "The Killing Moon",
+      "The World We Make",
+      "The Hundred Thousand Kingdoms",
+      "Emergency Skin"
+    ],
+    poc: true,
+    gender: "female",
+    lgbt: false,
+    fav: true,
+    country: [
+      {
+        code: "US",
+        name: "United States"
+      }
+    ],
+    website: "nkjemisin.com"
+  },
+  {
+    name: "Octavia E. Butler",
+    books: ["Bloodchild and Other Stories", "Kindred"],
+    poc: true,
+    gender: "female",
+    lgbt: false,
+    fav: true,
+    country: [
+      {
+        code: "US",
+        name: "United States"
+      }
+    ],
+    website: "octaviabutler.com"
+  },
+  {
+    name: "Xiran Jay Zhao",
+    books: ["Iron Widow"],
+    poc: true,
+    gender: "non-binary",
+    lgbt: true,
+    fav: false,
+    country: [
+      {
+        code: "CN",
+        name: "China"
+      },
+      {
+        code: "CA",
+        name: "Canada"
+      }
+    ],
+    website: "xiranjayzhao.com"
+  },
+  {
+    name: "Nnedi Okorafor",
+    books: ["Binti", "Binti: Home", "Binti: Night Masquerade", "Akata Witch"],
+    poc: true,
+    gender: "female",
+    lgbt: false,
+    fav: false,
+    country: [
+      {
+        code: "NG",
+        name: "Nigeria"
+      },
+      {
+        code: "US",
+        name: "United States"
+      }
+    ],
+    website: "nnedi.com"
+  },
+  {
+    name: "Rebecca F. Kuang",
+    books: ["The Poppy War", "The Dragon Republic", "The Burning God", "Babel"],
+    poc: true,
+    gender: "female",
+    lgbt: false,
+    fav: true,
+    country: [
+      {
+        code: "CN",
+        name: "China"
+      },
+      {
+        code: "US",
+        name: "United States"
+      }
+    ],
+    website: "rfkuang.com"
+  },
+  {
+    name: "Namina Forna",
+    books: ["The Gilded Ones"],
+    poc: true,
+    gender: "female",
+    lgbt: false,
+    fav: false,
+    country: [
+      {
+        code: "SL",
+        name: "Sierra Leone"
+      },
+      {
+        code: "US",
+        name: "United States"
+      }
+    ],
+    website: "naminaforna.com"
+  },
+  {
+    name: "Rivers Solomon",
+    books: ["An Unkindness of Ghosts", "The Deep"],
+    poc: true,
+    gender: "non-binary",
+    lgbt: true,
+    fav: false,
+    country: [
+      {
+        code: "US",
+        name: "United States"
+      }
+    ],
+    website: "rivers-solomon.com"
+  },
+  {
+    name: "Trevor Noah",
+    books: ["Born a Crime"],
+    poc: true,
+    gender: "male",
+    lgbt: false,
+    fav: false,
+    country: [
+      {
+        code: "ZA",
+        name: "South Africa"
+      }
+    ],
+    website: "trevornoah.com"
+  },
+  {
+    name: "Talia Hibbert",
+    books: ["Get a Life, Chloe Brown"],
+    poc: true,
+    gender: "female",
+    lgbt: true,
+    fav: false,
+    country: [
+      {
+        code: "GB",
+        name: "United Kingdom"
+      }
+    ],
+    website: "taliahibbert.com"
+  },
+  {
+    name: "June Hur",
+    books: ["The Red Palace"],
+    poc: true,
+    gender: "female",
+    lgbt: false,
+    fav: false,
+    country: [
+      {
+        code: "KR",
+        name: "Korea"
+      },
+      {
+        code: "CA",
+        name: "Canada"
+      }
+    ],
+    website: "junehur.com"
+  },
+  {
+    name: "Colson Whitehead",
+    books: ["The Underground Railroad", "The Nickel Boys"],
+    poc: true,
+    gender: "male",
+    lgbt: false,
+    fav: true,
+    country: [
+      {
+        code: "US",
+        name: "United States"
+      }
+    ],
+    website: "colsonwhitehead.com"
+  },
+  {
+    name: "Fonda Lee",
+    books: ["Jade City"],
+    poc: true,
+    gender: "female",
+    lgbt: false,
+    fav: false,
+    country: [
+      {
+        code: "US",
+        name: "United States"
+      },
+      {
+        code: "CA",
+        name: "Canada"
+      },
+      {
+        code: "CN",
+        name: "China"
+      }
+    ],
+    website: "fondalee.com"
+  },
+  {
+    name: "Axie Oh",
+    books: ["The Girl Who Fell Beneath the Sea"],
+    poc: true,
+    gender: "female",
+    lgbt: false,
+    fav: false,
+    country: [
+      {
+        code: "US",
+        name: "United States"
+      },
+      {
+        code: "KR",
+        name: "Korea"
+      }
+    ],
+    website: "axieoh.com"
+  },
+  {
+    name: "Sidik Fofana",
+    books: ["Stories from the Tenants Downstairs"],
+    poc: true,
+    gender: "male",
+    lgbt: false,
+    fav: false,
+    country: [
+      {
+        code: "US",
+        name: "United States"
+      }
+    ],
+    website: "sidikfofana.com"
+  },
+  {
+    name: "Krystal Marquis",
+    books: ["The Davenports"],
+    poc: true,
+    gender: "female",
+    lgbt: false,
+    fav: false,
+    country: [
+      {
+        code: "US",
+        name: "United States"
+      }
+    ],
+    website: "krystalmarquis.com"
+  }
+];
